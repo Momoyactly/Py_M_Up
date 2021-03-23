eb5d535ef59a58687e76a080e1f7b3281050531a
Uriel Arriaga <uriel.arriaga@momodevops.dev>
2021-03-22 19:52:25 -0600

    Finish work

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,41 +1,30 @@
 import csv
 import os
-import calendar
 
 m =[]
-with open(os.getcwd() + "/PyBank/Resources/budget_data.csv", mode='r') as csv:
-    for counter, row in enumerate(csv):
-        m.append(row.split(","))
+with open(os.getcwd() + "/PyBank/Resources/budget_data.csv", mode='r') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csv_reader)
+    for row in csvfile:
+        date, profit = row.split(",")
+        m.append([date,int(profit.replace("\n",""))])
 
-headers = m[0]
-m.pop(0)
+
 csv_in_array = [[m[j][i] for j in range(len(m))] for i in range(len(m[0]))] 
-for i, profit_in_month in enumerate(csv_in_array[1]):
-   csv_in_array[1][i] = int(profit_in_month[:-2])
 
-
-first_month, first_year = csv_in_array[0][0].split("-")
-last_month, last_year   = csv_in_array[0][-1].split("-")
 Analysis = ["Financial Analysis", "----------------------------"]
-Analysis.append("Total months: "+str(list(calendar.month_abbr).index(last_month)
-                                + 12*int(last_year)
-                                - list(calendar.month_abbr).index(first_month)
-                                - 12*int(first_year)
-                                + 1))
+Analysis.append(f"Total months: {len(csv_in_array[0])}")
+Analysis.append(f"Total: $ {sum(csv_in_array[1])}")
+Analysis.append(f"Average Change: ${sum(csv_in_array[1])/len(csv_in_array[1])}")
 
+greatest = []
+i_Max_increase = csv_in_array[1].index(max(csv_in_array[1]))
+i_Min_decrese = csv_in_array[1].index(min(csv_in_array[1]))
+greatest.append([csv_in_array[0][i_Max_increase],csv_in_array[1][i_Max_increase]])
+greatest.append([csv_in_array[0][i_Min_decrese],csv_in_array[1][i_Min_decrese]])
 
-Analysis.append("total: $"+str(sum(csv_in_array[1])))
-
-Analysis.append("Average Change: $"
-                + str(sum(csv_in_array[1])/len(csv_in_array[1])))
-
-Analysis.append("Greatest Increase in Profits: "
-    + csv_in_array[0][csv_in_array[1].index(max(csv_in_array[1]))] + " $("
-    + str(csv_in_array[1][csv_in_array[1].index(max(csv_in_array[1]))])+")")
-
-Analysis.append("Greatest Increase in Profits: "
-    + csv_in_array[0][csv_in_array[1].index(min(csv_in_array[1]))] + " $("
-    + str(csv_in_array[1][csv_in_array[1].index(min(csv_in_array[1]))])+")")
+Analysis.append(f"Greatest Increase in Profits: {greatest[0][0]} $({greatest[0][1]})")
+Analysis.append(f"Greatest decrese in Profits: {greatest[1][0]} $({greatest[1][1]})")
 
 with open(os.getcwd() + "/PyBank/Analysis/Analysis.txt","w+") as file:
     for row in Analysis :
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,12 +2,12 @@ import csv
 import os
 
 m =[]
-with open(os.getcwd() + "/PyPoll/Resources/election_data.csv", mode='r') as csv:
-    for counter, row in enumerate(csv):
+with open(os.getcwd() + "/PyPoll/Resources/election_data.csv", mode='r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+    for counter, row in enumerate(csvfile):
         m.append(row.split(","))
 
-headers = m[0]
-m.pop(0)
 Analysis = ["Election Results", "-------------------------"]
 Analysis.append("Total votes: " + str(len(m)))
 Analysis.append("-------------------------")
@@ -17,7 +17,7 @@ csv_in_array = [[m[j][i] for j in range(len(m))] for i in range(len(m[0]))]
 count_by_candidate = {}
 for name in set(csv_in_array[2]):
     count_by_candidate[name[:-1]] = [csv_in_array[2].count(name),
-                    round((csv_in_array[2].count(name)/len(csv_in_array[2]))*100)]
+        round((csv_in_array[2].count(name)/len(csv_in_array[2]))*100)]
 
 max_votes = ["",0]
 for candidate, value in count_by_candidate.items():
